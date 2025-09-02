281495b88f646846c539bd06b621334d80a392f4
Shtromi <cadrian111@gmail.com>
2025-09-02 15:11:39 +0300

    Create 2.2 — Function return values (value-returning functions).cpp

---
+++ 2.2 — Function return values (value-returning functions).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int getValueFromUser()
+{
+ 	std::cout << "Enter an integer: ";
+	int input{};
+	std::cin >> input;
+	//std::cout << input;
+    return input;
+}
+
+int main() {
+  
+  int x{};
+  x = getValueFromUser();   
+  std::cout << "The double of the entered number is :" << x*2;
+  
+//  return 0;
+}
